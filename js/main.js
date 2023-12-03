93f7ce0128c897de59fea9de9b2f83fd8d3bfe30
rainbowparty <152808721+rainbowparty@users.noreply.github.com>
2023-12-03 15:31:42 -0500

    commit

---
+++ js/main.js
@@ -0,0 +1,85 @@
+/*!
+* Start Bootstrap - Agency v7.0.11 (https://startbootstrap.com/theme/agency)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+window.sr = ScrollReveal();
+
+sr.reveal('.titulo', {
+    duration: 4000,
+    origin: 'bottom',
+    distance: '-200px',
+});
+
+sr.reveal('.sub', {
+    duration: 4000,
+    origin: 'top',
+    distance: '-200px',
+});
+
+sr.reveal('.linea', {
+    duration: 2500,
+    
+});
+
+sr.reveal('.bala-iz', {
+    duration: 4000,
+    origin: 'rign',
+    distance: '-300px',
+});
+
+sr.reveal('.bala-de', {
+    duration: 4000,
+    origin: 'left',
+    distance: '-300px',
+});
